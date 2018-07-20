d0ce8c2486e2a5a644782a994a0c1017a4d4c0cf
bartlomiej-kedziora <bartlomiej.kedziora@vp.pl>
2018-07-20 11:46:05 +0200

    task 7.1 - poemBeautifier

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
@@ -7,6 +8,9 @@ import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
+
+        //module 7
+        System.out.println("----------MODULE 7----------");
         Processor processor = new Processor();
         Executor codeToExecute = () -> System.out.println("This is an example text.");
         processor.execute(codeToExecute);
@@ -22,5 +26,13 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        // task 7.1
+        System.out.println("----------TASK 7.1----------");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("example", text -> "ABC" + text);
+        poemBeautifier.beautify("example2", text -> text.toUpperCase());
+        poemBeautifier.beautify("EXAMPLE3", text -> text.toLowerCase());
+        poemBeautifier.beautify("example4", text -> text.subSequence(3,6).toString());
     }
 }
