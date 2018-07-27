469ac1f387b29f4111e7e6aae9ff532ff84a52b9
bartlomiej-kedziora <bartlomiej.kedziora@vp.pl>
2018-07-27 18:11:00 +0200

    task 8.3 Second Challenge

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -3,11 +3,14 @@ package com.kodilla.exception.main;
 import com.kodilla.exception.io.FileReader;
 import com.kodilla.exception.io.FileReaderException;
 import com.kodilla.exception.io.FileReaderWithoutHandling;
+import com.kodilla.exception.test.ExceptionHandling;
+import com.kodilla.exception.test.SecondChallenge;
 
 import java.io.IOException;
 
 public class ExceptionModuleRunner {
     public static void main(String[] args) {
+        System.out.println("----------task 8.3.1----------");
         FileReader fileReader = new FileReader();
         try {
             fileReader.readFile();
@@ -15,6 +18,7 @@ public class ExceptionModuleRunner {
             System.out.println("Problem while reading a file!");
         }
 
+        System.out.println("----------task 8.3.2----------");
         FileReaderWithoutHandling fileReader2 = new FileReaderWithoutHandling();
         try {
             fileReader2.readFile();
@@ -22,5 +26,8 @@ public class ExceptionModuleRunner {
             System.out.println("Ups sth went wrong!, Error:" + e);
         }
 
+        System.out.println("----------task 8.3.3----------");
+        ExceptionHandling exceptionHandling = new ExceptionHandling(new SecondChallenge());
+        System.out.println(exceptionHandling.test(2.0, 1.0));
     }
 }
