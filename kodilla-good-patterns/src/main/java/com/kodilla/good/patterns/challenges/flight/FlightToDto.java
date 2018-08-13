31f9f201e1a75b153ee3266e29809372709ddeca
bartlomiej-kedziora <bartlomiej.kedziora@vp.pl>
2018-08-13 20:24:25 +0200

    task 9.5 Flight search - update

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flight/FlightToDto.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flight/FlightToDto.java
@@ -1,11 +1,10 @@
 package com.kodilla.good.patterns.challenges.flight;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class FlightToDto {
-    private List<City> fromDirectCities = new ArrayList<>();
-    private List<City> viaCities = new ArrayList<>();
+    private List<City> fromDirectCities;
+    private List<City> viaCities;
 
     public FlightToDto(List<City> fromDirectCities, List<City> viaCities) {
         this.fromDirectCities = fromDirectCities;
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flight/SearchService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flight/SearchService.java
@@ -1,8 +1,8 @@
 package com.kodilla.good.patterns.challenges.flight;
 
 public interface SearchService {
-    public Flight searchFrom(City city);
+    Flight searchFrom(City city);
 
-    public FlightToDto searchTo(City city);
+    FlightToDto searchTo(City city);
 
 }
