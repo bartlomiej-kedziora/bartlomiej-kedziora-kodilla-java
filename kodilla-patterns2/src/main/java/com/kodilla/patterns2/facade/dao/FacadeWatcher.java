338fb8f45c4b42c46ee8c6df01e3ccd0fabbe54a
bartlomiej-kedziora <bartlomiej.kedziora@vp.pl>
2018-10-20 13:42:15 +0200

    module 20 - aspcts programming in facade pattern, task - 20.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/dao/FacadeWatcher.java
@@ -0,0 +1,36 @@
+package com.kodilla.patterns2.facade.dao;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class FacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(com.kodilla.patterns2.facade.dao.FacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent() {
+        LOGGER.info("Method starts execution");
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("The consumed: " + (end - begin) + "[ms]");
+        } catch(Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
+}
